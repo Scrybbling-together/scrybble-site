8eabf88750b1e291690ae1356a9dcc3fb8e0762f
Laura Brekelmans <mail@laura.fm>
2025-05-09 18:29:37 +0200

    Add signups chart

--- app/Filament/Widgets/SignupsChart.php
+++ app/Filament/Widgets/SignupsChart.php
@@ -3,30 +3,32 @@
 namespace App\Filament\Widgets;
 
 use App\Models\User;
-use DB;
 use Filament\Widgets\ChartWidget;
 use Flowframe\Trend\Trend;
 use Flowframe\Trend\TrendValue;
+use Illuminate\Support\Carbon;
 
 class SignupsChart extends ChartWidget
 {
-    protected static ?string $heading = 'Chart';
+    protected static ?string $heading = 'Signups';
 
     protected function getData(): array
     {
-        $data = Trend::model(User::class)->between(start: User::query()->first()->created_at, end: now())->perMonth()->count();
+        $data = Trend::model(User::class)->between(start: User::query()->first()->created_at, end: now())->perMonth()->count("created_at");
+//        dd($data->map(fn (TrendValue $value) => ["x" => $value->date, "y" => $value->aggregate])->toArray());
         return [
             "datasets" => [
                 [
-                    'label' => "Signups",
-                    'data' => $data->map(fn (TrendValue $value) => ["x" => $value->date, "y" => $value->aggregate])
+                    'label' => "Signups per month",
+                    'data' => $data->map(fn (TrendValue $value) => $value->aggregate)
                 ]
             ],
+            "labels" => $data->map(fn (TrendValue $value) => $value->date)
         ];
     }
 
     protected function getType(): string
     {
-        return 'scatter';
+        return 'line';
     }
 }
