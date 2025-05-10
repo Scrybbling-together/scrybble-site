f44d1caa497d60ca619de4a7fd65160fe58926e1
Laura Brekelmans <mail@laura.fm>
2025-05-10 23:10:37 +0200

    Add ".." path to RMFiletreeController.php

--- app/Http/Controllers/RMFiletreeController.php
+++ app/Http/Controllers/RMFiletreeController.php
@@ -3,16 +3,24 @@
 namespace App\Http\Controllers;
 
 use App\Services\RMapi;
+use Eloquent\Pathogen\AbsolutePath;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Arr;
 
 class RMFiletreeController extends Controller {
 
     public function index(Request $request, RMapi $rmapi): JsonResponse {
         $path = $request->get('path') ?? '/';
 
+        $items = $rmapi->list($path);
+        if ($path !== "/") {
+            $parent = AbsolutePath::fromString($path)->parent()->normalize()->string();
+            $items->prepend(['type' => 'd', 'name' => '..', 'path' => $parent]);
+        }
+
         return response()->json([
-            "items" => $rmapi->list($path),
+            "items" => $items,
             "cwd" => $path
         ]);
     }
