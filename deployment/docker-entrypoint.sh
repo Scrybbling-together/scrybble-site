3883d6775872bdefebf2967710c9539408bcbd65
Laura Brekelmans <mail@laura.fm>
2026-06-03 16:46:58 +0200

    Remove redundant migrate cmd

--- deployment/docker-entrypoint.sh
+++ deployment/docker-entrypoint.sh
@@ -3,7 +3,6 @@ set -e
 
 php /var/www/html/artisan optimize:clear
 php /var/www/html/artisan optimize
-php /var/www/html/artisan migrate --force
 
 if [ "${SCRYBBLE_AUTO_MIGRATE:-true}" = "true" ] && [[ "$*" == *apache2-foreground* ]]; then
     php /var/www/html/artisan migrate --force
