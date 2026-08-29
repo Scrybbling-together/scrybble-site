5ec605242496b38c55a0abc8677d9dd62fb70fd1
Laura Brekelmans <mail@laura.fm>
2026-08-29 20:08:28 +0200

    Re-add breeze frontend defaults for non-legacy files

---
+++ resources/views/auth/verify-email.blade.php
@@ -0,0 +1,41 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container m-4">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">{{ __('Verify Email Address') }}</div>
+
+                <div class="card-body">
+                    <p class="mb-3">
+                        {{ __('Thanks for signing up! Before getting started, could you verify your email address by clicking on the link we just emailed to you? If you didn\'t receive the email, we will gladly send you another.') }}
+                    </p>
+
+                    @if (session('status') == 'verification-link-sent')
+                        <div class="alert alert-success" role="alert">
+                            {{ __('A new verification link has been sent to the email address you provided during registration.') }}
+                        </div>
+                    @endif
+
+                    <div class="d-flex justify-content-between align-items-center">
+                        <form method="POST" action="{{ route('verification.send') }}" class="d-inline">
+                            @csrf
+
+                            <button type="submit" class="btn btn-primary">
+                                {{ __('Resend Verification Email') }}
+                            </button>
+                        </form>
+
+                        <form method="POST" action="{{ route('logout') }}" class="d-inline">
+                            @csrf
+
+                            <button type="submit" class="btn btn-link p-0">
+                                {{ __('Log Out') }}
+                            </button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
